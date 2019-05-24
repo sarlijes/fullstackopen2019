f95d6075e3616a002cc7f4aadb255159ecb5560e
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-05-24 21:12:29 +0300

    Anecdotes, step 1

--- chapter_1/anecdotes/src/index.js
+++ chapter_1/anecdotes/src/index.js
@@ -4,35 +4,30 @@ import './index.css';
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
+    const [points, setAll] = useState([].Array.apply(null, Array(6)).map(Number.prototype.valueOf, 0))
+
+    const handleVote = selected => {
+        const copy = [...points]
+        copy[selected] += 1
+        setAll(copy)
+    }
 
     return (
         <div>
             <div className="App">
                 <header className="App-header">
                     <Display anecdote={props.anecdotes[selected]} votes={points[selected]} />
-                    <Button handleClick={() => vote(selected)} text="Vote" />
+                    <Button handleClick={() => handleVote(selected)} text="Vote" />
                     <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="Next one" />
-                    
                 </header>
             </div>
         </div>
     )
 }
 
-
 const Button = (props) => (
     <button onClick={props.handleClick}>{props.text}</button>
 )
-
-const setSelected = randomNumber => {
-    setSelected(randomNumber)
-}
-
-const vote = selected => {
-    const copy = [...points]
-    copy[selected] += 1
-    points = copy
-}
 
 const anecdotes = [
     'If it hurts, do it more often',
@@ -43,7 +38,7 @@ const anecdotes = [
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-let points = Array.apply(null, Array(6)).map(Number.prototype.valueOf, 0);
+// let points = Array.apply(null, Array(6)).map(Number.prototype.valueOf, 0);
 
 const Display = (props) => {
     return (
