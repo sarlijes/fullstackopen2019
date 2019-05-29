516903cb1511823031685ff758acf0f290e34c64
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-05-29 20:06:13 +0300

    Chapter 2: course info, step 8

--- chapter_2/02_course_info/src/App.js
+++ chapter_2/02_course_info/src/App.js
@@ -14,49 +14,65 @@ const Total = props => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Konsolin käyttö',
-        exercises: 594,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  const oneCourse = (course) => 
+    <div>
+      <Header name={course.name} />
+      <ul>
+        {partRow(course)}
+      </ul>
+      <Total parts={course.parts} />
+    </div>
+  
 
-  const rows = () => course.parts.map(course =>
+  const partRow = (course) => course.parts.map(course =>
     <Course
       key={course.id}
       course={course}
     />
   )
 
-  const result = course.parts.map(note => note.id)
-  console.log(result)
-
   return (
     <div>
-      <Header name={course.name} />
-      <ul>
-        {rows()}
-      </ul>
-      <Total parts={course.parts} />
+      {courses.map(course => <div>{oneCourse(course)}</div>)}
     </div>
   )
 }
