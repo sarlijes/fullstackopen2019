b82600d5af055d133cf6a0b848749b3515746b65
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-06-19 20:30:43 +0300

    4.6 initial commit

--- chapter_4/04-bloglist/utils/list_helper.js
+++ chapter_4/04-bloglist/utils/list_helper.js
@@ -1,7 +1,60 @@
+let _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const count = (blogs) => {
+    return blogs.length
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce(reducer, 0)
+}
+
+const favouriteBlog = (blogs) => {
+    const blogWithMostVotes = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+    return blogWithMostVotes
+}
+
+const authorWithMostPosts = (blogs) => {
+
+    const reducer = (sum, item) => { return sum + item }
+
+    const authors = _.groupBy(blogs, 'author')
+    console.log(authors.length)
+    // authors.forEach (el =>
+    //     console.log(el))
+    _.forEach(authors, function(el) {
+        // console.log(el.length);
+        // console.log(el.author);
+      });
+    return authors
+
+}
+module.exports = {
+    dummy, count, totalLikes, favouriteBlog, authorWithMostPosts
+}
+
+/*
+    // console.log('---- ', _.groupBy(authors.forEach))
+    // console.log(authors.forEach(el =>
+    //     console.log(el)))
+    _.forEach(authors, function (el) {
+        console.log(authors)
+        _.reduce(authors, function (sum, n) {
+            console.log("el nyt ---- ", el)
+            // return sum + el.length
+            // }, 0)
+            // )
+        })
+
+    }
+//    return authors
+}
+*/
