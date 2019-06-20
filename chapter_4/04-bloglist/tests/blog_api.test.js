b8446389e7ac20060991037a9e7469abf2db9a35
Jessica Sarlin <jessica.sarlin@gmail.com>
2019-06-20 13:44:44 +0300

    4.10 test to add one blog post

--- chapter_4/04-bloglist/tests/blog_api.test.js
+++ chapter_4/04-bloglist/tests/blog_api.test.js
@@ -2,35 +2,19 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-    },
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    }
-]
+
 
 beforeEach(async () => {
     await Blog.deleteMany({})
 
-    let blogObject = new Blog(initialBlogs[0])
+    let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
 
-    blogObject = new Blog(initialBlogs[1])
+    blogObject = new Blog(helper.initialBlogs[1])
     await blogObject.save()
 })
 
@@ -41,12 +25,18 @@ test('blog posts are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
-test('there are two blogs', async () => {
+test('the two initial blogs are returned', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(initialBlogs.length)
+    expect(response.body.length).toBe(helper.initialBlogs.length)
 })
 
-test('the first blog is about React patterns ', async () => {
+test('a specific blog post is within the returned data', async () => {
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(r => r.title)
+    expect(contents).toContain('React patterns')
+})
+
+test('the first blog post is about React patterns ', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body[0].title).toBe('React patterns')
 })
@@ -56,6 +46,51 @@ test('the blog post object has an id defined (instead if an "__id")', async () =
     expect(response.body.id).toBeDefined
 })
 
+test('adding one blog post increases the amount of posts', async () => {
+
+    const newBlog =
+    {
+        title: 'async/await simplifies making async calls',
+        author: "Jane Doe",
+        url: "https://not.here",
+        likes: 73,
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(blog => blog.title)
+    expect(response.body.length).toBe(helper.initialBlogs.length + 1)
+    expect(titles).toContain('async/await simplifies making async calls')
+})
+/*
+
+      const blogsAtEnd = await helper.blogInDb()
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length + 1)
+
+      const contents = blogsAtEnd.map(b => b.content)
+      expect(contents).toContain('async/await simplifies making async calls')
+    })
+
+    test('fails with status code 400 if data invailid', async () => {
+      const newBlog = {
+        important: true
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd.length).toBe(helper.initialBlogs.length)
+    })
+  })
+*/
 afterAll(() => {
     mongoose.connection.close()
 })
